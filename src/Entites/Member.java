0f7a4608ceba919d579f9ff47fd7f6bcca45f359
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:25:35 +0400

    Adding Constructor for member

--- src/Entites/Member.java
+++ src/Entites/Member.java
@@ -7,4 +7,11 @@ public class Member {
     private String name;
     private Address address;
     private List borrowItem;
+
+    public Member(String ID, String name, Address address, List borrowItem) {
+        this.ID = ID;
+        this.name = name;
+        this.address = address;
+        this.borrowItem = borrowItem;
+    }
 }
