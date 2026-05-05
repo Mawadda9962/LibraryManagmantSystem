6e2cf5f7662ccb3f32a073721eb5341d783d5ac2
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:24:08 +0400

    Constructor for Address

--- src/Entites/Address.java
+++ src/Entites/Address.java
@@ -5,6 +5,9 @@ public class Address {
     private String City;
     private String postalCode;
 
-
-
+    public Address(String street, String city, String postalCode) {
+        Street = street;
+        City = city;
+        this.postalCode = postalCode;
+    }
 }
