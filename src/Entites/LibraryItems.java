f6e0ab9767da629f1bcee60d6e35fcc1b63f07cf
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:16:26 +0400

    Adding getter and setter

--- src/Entites/LibraryItems.java
+++ src/Entites/LibraryItems.java
@@ -5,10 +5,36 @@ public class LibraryItems {
     private String Tittle;
     private boolean Status;
 
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTittle() {
+        return Tittle;
+    }
+
+    public void setTittle(String tittle) {
+        Tittle = tittle;
+    }
+
+    public boolean isStatus() {
+        return Status;
+    }
+
+    public void setStatus(boolean status) {
+        Status = status;
+    }
+
     public LibraryItems(String ID, String tittle, boolean status) {
         this.ID = ID;
         Tittle = tittle;
         Status = status;
+
+
 
 
     }
