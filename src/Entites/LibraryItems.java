84ed1d305f583469bc71173481ee50fab066b261
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:16:03 +0400

    Adding Constructor for LibraryItem

--- src/Entites/LibraryItems.java
+++ src/Entites/LibraryItems.java
@@ -4,4 +4,12 @@ public class LibraryItems {
     private String ID;
     private String Tittle;
     private boolean Status;
+
+    public LibraryItems(String ID, String tittle, boolean status) {
+        this.ID = ID;
+        Tittle = tittle;
+        Status = status;
+
+
+    }
 }
