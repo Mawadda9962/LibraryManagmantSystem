a8656353a4e0304a7c6ebf0131544fb76b032232
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:09:48 +0400

    Adding constructor for book entity

--- src/Entites/Books.java
+++ src/Entites/Books.java
@@ -2,5 +2,27 @@ package Entites;
 
 public class Books {
 
-    private
+    private String Author;
+    private String isbn;
+
+    public Books(String author, String isbn) {
+        Author = author;
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
