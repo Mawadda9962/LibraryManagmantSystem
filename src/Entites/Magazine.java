e51095c350eff93e41b25931722e76ec84474ee8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-05 10:12:44 +0400

    Adding getter and setter

--- src/Entites/Magazine.java
+++ src/Entites/Magazine.java
@@ -4,5 +4,26 @@ public class Magazine {
     private String issueNumber;
     private String publisher;
 
+    public String getIssueNumber() {
+        return issueNumber;
+    }
 
+    public void setIssueNumber(String issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Magazine(String issueNumber, String publisher) {
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+
+
+    }
 }
